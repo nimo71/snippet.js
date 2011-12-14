fa912a97d55ab47bec42b7a63542d1992a59b3cd
Nick Monk <nick@monksoftware.co.uk>
2011-12-14 00:35:04 +0000

    Code cleanup

--- snippet.js
+++ snippet.js
@@ -7,9 +7,9 @@ var Step = require('step');
 
 exports.processRequest = function (req, res) {
 	console.log('snippet.processRequest');
-	var html = null;
 	Step(
 		function checkHtmlExists() {
+			//TODO: refactor and use _.memoize to cache HTML exists check
 			console.log('checkHtmlExists()');
 			var filepath = './html' + req.url;
 		    if (filepath == './') filepath = './html/index.htm';
@@ -29,76 +29,75 @@ exports.processRequest = function (req, res) {
 		    return filepath;
 		},
 		function loadHtml(err, filepath) {
-			console.log(err);
 			if (err) throw err;
 			console.log('loading html, filepath: ' + filepath);
+			// TODO: refactor and use _.memoize to cache loading of HTML
 			fs.readFile(filepath, this);
 		},
-		function processSnippets(err, content) {
-			console.log(err);
+		function jQueryfy(err, content) {
 			if (err) throw err;
-			console.log('processing snippets...');
-            var document = jsdom.jsdom(content);
-            var window = document.createWindow();
-            console.log('about to step');
-            Step(
-            	function () {
-            		console.log('jQueryfyDom...');
-            		jsdom.jQueryify(window, this);
-            	},
-            	function (err) {
-            		// load and process snippets in the order declared in the class attribute 
-            		console.log('loadAndProcessSnippets()');
-	            	window.$('[class^="snippet-"]').each(function(index, elem) {
-	            		
-	            		var classList = window.$(elem).attr('class').split(/\s+/);
-	            		var snippetList = _(classList).filter(function (className) {
-	            			return className.match(/^snippet-.*/);
-	            		});
-	            		var tagname = elem.nodeName.toLowerCase();
-	            		console.log('processing: '+ snippetList +' on element '+ tagname);
-	            		
-	            		_(snippetList).each(function (snippetName) 
-	            		{
-	            			var sname = snippetName.split('-')[1];
-	            			console.log('loading snippet: ' + sname);
-			            	
-	            			// find the snippet in the path and process
-	            			_(context.path).each(function (dir) {
-	            				var snippetPath = dir + context.separator + sname +'.js';
-	            				console.log('looking for snippet: ' + snippetPath);
-			 					
-	            				if (!path.existsSync(snippetPath)) throw 500;
-	            				
-	            				// TODO: use _.memoize to cache snippets
-	        					var snippet = require(snippetPath);
-	        					snippet.process(context, window.$, elem);
-	        					console.log('processed snippet, content: ' + window.$(elem).html());
-	            			});
-	            		});
-            		});// each
-	            	return window.document.innerHTML;
-	            }, 
-	            function sendResponse(err, html) {
-	    			if (err) {
-	    				console.log('error: ' + err.message);
-	    				res.writeHead(err.code);
-	    	            res.end();
-	    	            return;
-	    			}
-	    			console.log('sending response: ' + html);
-	    	        res.writeHead(200, {'Content-Type': 'text/html'}); 
-	    			res.end(context.doctype + html);
-	    		}
-	        );
+		    var document = jsdom.jsdom(content);
+		    jqueryfy(document.createWindow(), this);
 		},
-		function sendResponse(err) {
+		function processSnippets(err, window) {
+			console.log('processing snippets...');
+			if (err) throw err;
+			
+    	    var $ = window.$;
+			$('[class^="snippet-"]').each(function (index, elem) {	
+	    		var classList = $(elem).attr('class').split(/\s+/);
+	    		var snippetList = _(classList).filter(function (className) {
+	    			return className.match(/^snippet-.*/);
+	    		});
+	    		var tagname = elem.nodeName.toLowerCase();
+	    		console.log('processing: '+ snippetList +' on element '+ tagname);
+	    		
+	    		_(snippetList).each(function (snippetName) {
+	    			var sname = snippetName.split('-')[1];
+	    			//TODO: use _.memoize to cache snippet loading?
+	    			var snippet = loadSnippet(sname);
+	    			snippet.process(context, $, elem);
+	    			console.log('processed snippet, content: ' + $(elem).html());
+	    		});
+	    	});
+		    return window.document.innerHTML;
+		},
+		function sendResponse(err, html) {
 			if (err) {
-				console.log('error: ' + err.message);
+				console.log('error: ' + err);
 				res.writeHead(err.code);
 	            res.end(err.message);
 	            return;
 			}
+			console.log('sending response: ' + html);
+	        res.writeHead(200, {'Content-Type': 'text/html'}); 
+			res.end(context.doctype + html);
 		}
 	);
 };
+
+function jqueryfy(window, processSnippets) {
+	jsdom.jQueryify(window, function() {
+		processSnippets(null, window);
+	});
+}
+
+function loadSnippet(snippetName) {
+	console.log('loading snippet: ' + snippetName);
+	
+	var snippet = null;
+	
+	// find the snippet in the path and process
+	_(context.path).each(function (dir) {
+		var snippetPath = dir + context.separator + snippetName +'.js';
+		console.log('looking for snippet: ' + snippetPath);
+		
+		if (!path.existsSync(snippetPath)) return true;
+		snippet = require(snippetPath);
+	});
+	if (snippet) return snippet;
+	throw {
+		code : 500, 
+		message : 'Snippet '+ snippetName +' not found'
+	};
+}
